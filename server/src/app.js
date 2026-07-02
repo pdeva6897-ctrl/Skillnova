db2d2489bf37351618ee1d0411a89ed8b7e4057e
Rajat Kumar <rajatkumar7861813@gmail.com>
2026-07-02 16:41:55 +0530

    feat(server): add request body size tracking

--- server/src/app.js
+++ server/src/app.js
@@ -22,6 +22,7 @@ import featuresRoutes, { publicApi as publicFeaturesRoutes } from './routes/feat
 import skillGapRoutes from './routes/skillGap.routes.js';
 import { etagMiddleware } from './utils/cache.js';
 import { requestId } from './middleware/requestId.js';
+import { bodySizeTracker } from './utils/metrics.js';
 
 const app = express();
 
@@ -60,6 +61,7 @@ app.use(
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 app.use(cookieParser());
+app.use(bodySizeTracker());
 
 // Logging
 if (!config.isProd) app.use(morgan('dev'));
